d673cafcfdd59ce334af985f3028eb342432e6bb
yantongyang <ycx005yty006@163.com>
2017-09-28 16:37:32 +0800

    init

---
+++ aiweier_web/src/main/java/com/awe/aiweier/AiweierApplication.java
@@ -0,0 +1,12 @@
+package com.awe.aiweier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiweierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiweierApplication.class, args);
+	}
+}
